27284f253114994e7d6821d16111ca4e474add71
Oliver Lemke <olemke@gmail.com>
2020-10-14 08:55:23 +0200

    Reformat and syntax fixes

--- controlfiles-python/instruments/metmm/sensor_descriptions/sensor_deimos.py
+++ controlfiles-python/instruments/metmm/sensor_descriptions/sensor_deimos.py
@@ -1,48 +1,49 @@
-# sensor_descriptions/sensor_deimos.arts   
+# sensor_descriptions/sensor_deimos.arts
 
-ws.Error( "The MetMM file for DEIMOS is not yet finalised!!!" )
+ws.Error("The MetMM file for DEIMOS is not yet finalised!!!")
 
 dlos = []
-    
-
-ws.MatrixSet(ws.antenna_dlos, np.array( [dlos] ).T )
-mm = [ [  23.8e9,  0.07e9,  0.0e6,   127e6 ],    #0 (1, V only)
-       [  23.8e9,  0.07e9,  0.0e6,   127e6 ],    #1 (1, H only)
-       [  50.1e9,  0.08e9,  0.0e6,    82e6 ],    #2 (2, V only)
-       [  50.1e9,  0.08e9,  0.0e6,    82e6 ]     #3 (2, H only)
 
 
-ws.MatrixSet(ws.met_mm_backend, np.array( mm ) )
+ws.MatrixSet(ws.antenna_dlos, np.array([dlos]).T)
+mm = [
+    [23.8e9, 0.07e9, 0.0e6, 127e6],  # 0 (1, V only)
+    [23.8e9, 0.07e9, 0.0e6, 127e6],  # 1 (1, H only)
+    [50.1e9, 0.08e9, 0.0e6, 82e6],  # 2 (2, V only)
+    [50.1e9, 0.08e9, 0.0e6, 82e6],  # 3 (2, H only)
+]
 
-ws.ArrayOfStringSet(ws.met_mm_polarisation, 
+ws.MatrixSet(ws.met_mm_backend, np.array(mm))
+
+ws.ArrayOfStringSet(
+    ws.met_mm_polarisation,
     [
-        "ISMAR-V", #0 (1, V only)
-        "ISMAR-H", #1 (1, H only)
-        "ISMAR-V", #2 (2, V only)
-        "ISMAR-H"  #3 (2, H only)
-    ])
-    
-#ws.VectorSet( ws.met_mm_antenna, "" )
-ws.Touch( ws.met_mm_antenna )
-
-# Number of frequencies for first accuracy (fast)
-ws.ArrayOfIndexSet(ws.met_mm_freq_number,
-    [   
-    12, #0 (1, V only)
-    12, #1 (1, H only)
-    12, #2 (2, V only)
-    12  #3 (2, H only)
-    ] )
-
-
-ws.VectorSet( 
-    ws.freq_spacing_tmp, 
-    np.array( [10e9, 1e9, 1e9, 1e9] )
+        "ISMAR-V",  # 0 (1, V only)
+        "ISMAR-H",  # 1 (1, H only)
+        "ISMAR-V",  # 2 (2, V only)
+        "ISMAR-H",  # 3 (2, H only)
+    ],
 )
 
-ws.Extract( ws.current_spacing, ws.freq_spacing_tmp, ws.met_mm_accuracy)
+# ws.VectorSet( ws.met_mm_antenna, "" )
+ws.Touch(ws.met_mm_antenna)
 
-ws.nrowsGet( ws.met_mm_nchannels, ws.met_mm_backend )
-ws.VectorSetConstant( ws.met_mm_freq_spacing, ws.met_mm_nchannels, 
-                         ws.current_spacing )
-ws.Delete(ws.current_spacing)   
+# Number of frequencies for first accuracy (fast)
+ws.ArrayOfIndexSet(
+    ws.met_mm_freq_number,
+    [
+        12,  # 0 (1, V only)
+        12,  # 1 (1, H only)
+        12,  # 2 (2, V only)
+        12,  # 3 (2, H only)
+    ],
+)
+
+
+ws.VectorSet(ws.freq_spacing_tmp, np.array([10e9, 1e9, 1e9, 1e9]))
+
+ws.Extract(ws.current_spacing, ws.freq_spacing_tmp, ws.met_mm_accuracy)
+
+ws.nrowsGet(ws.met_mm_nchannels, ws.met_mm_backend)
+ws.VectorSetConstant(ws.met_mm_freq_spacing, ws.met_mm_nchannels, ws.current_spacing)
+ws.Delete(ws.current_spacing)
