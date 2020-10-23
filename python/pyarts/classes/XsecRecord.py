70e9384d2fa0e6a3c1b2a105a81d1b8c60db5526
Oliver Lemke <olemke@gmail.com>
2020-10-23 15:27:22 +0200

    XsecRecord version 2 implementation
    Preparations for new crosssection fit algorithm.
    Tighten compare criteria for xsec test.
    Simplify fit code.
    Adapt for changed grid order.

--- python/pyarts/classes/XsecRecord.py
+++ python/pyarts/classes/XsecRecord.py
@@ -2,6 +2,7 @@ import ctypes as c
 from collections.abc import Sized
 from pyarts.workspace.api import arts_api as lib
 
+from pyarts.classes.GriddedField2 import ArrayOfGriddedField2
 from pyarts.classes.Vector import Vector, ArrayOfVector
 from pyarts.classes.SpeciesTag import SpeciesTag
 from pyarts.classes.io import correct_save_arguments, correct_read_arguments
@@ -12,6 +13,9 @@ class XsecRecord:
     """ ARTS XsecRecord data
 
     Properties:
+        version:
+            Version (Index)
+
         spec:
             Species (Index)
 
@@ -35,6 +39,21 @@ class XsecRecord:
 
         temperature_intersect:
             Intersect of temperature fits (ArrayOfVector)
+
+        fitminpressures:
+            Mininum pressures of fit
+
+        fitmaxpressures:
+            Maximum pressures of fit
+
+        fitmintemperatures:
+            Mininum temperatures of fit
+
+        fitmaxtemperatures:
+            Mininum temperatures of fit
+
+        fitcoeffs:
+            Fit coefficients
     """
 
     def __init__(self, data=None):
@@ -46,6 +65,15 @@ class XsecRecord:
             self.__data__ = c.c_void_p(lib.createXsecRecord())
             if data is not None:
                 raise RuntimeError("Only supports void initialization")
+
+    @property
+    def version(self):
+        """ Version (Index) """
+        return lib.getVersionXsecRecord(self.__data__)
+
+    @version.setter
+    def version(self, val):
+        lib.setVersionXsecRecord(self.__data__, int(val))
 
     @property
     def spec(self):
@@ -122,6 +150,51 @@ class XsecRecord:
     def temperature_intersect(self, val):
         self.temperature_intersect.set(val)
 
+    @property
+    def fitminpressures(self):
+        """ Minimum pressures of fit (Vector) """
+        return Vector(c.c_void_p(lib.getFitMinPressuresXsecRecord(self.__data__)))
+
+    @fitminpressures.setter
+    def fitminpressures(self, val):
+        self.fitminpressures.set(val)
+
+    @property
+    def fitmaxpressures(self):
+        """ Maximum pressures of fit (Vector) """
+        return Vector(c.c_void_p(lib.getFitMaxPressuresXsecRecord(self.__data__)))
+
+    @fitmaxpressures.setter
+    def fitmaxpressures(self, val):
+        self.fitmaxpressures.set(val)
+
+    @property
+    def fitmintemperatures(self):
+        """ Minimum temperatures of fit (Vector) """
+        return Vector(c.c_void_p(lib.getFitMinTemperaturesXsecRecord(self.__data__)))
+
+    @fitmintemperatures.setter
+    def fitmintemperatures(self, val):
+        self.fitmintemperatures.set(val)
+
+    @property
+    def fitmaxtemperatures(self):
+        """ Maximum temperatures of fit (Vector) """
+        return Vector(c.c_void_p(lib.getFitMaxTemperaturesXsecRecord(self.__data__)))
+
+    @fitmaxtemperatures.setter
+    def fitmaxtemperatures(self, val):
+        self.fitmaxtemperatures.set(val)
+
+    @property
+    def fitcoeffs(self):
+        """ Fit coefficients (ArrayOfGriddedField2) """
+        return ArrayOfGriddedField2(c.c_void_p(lib.getFitCoeffsXsecRecord(self.__data__)))
+
+    @fitcoeffs.setter
+    def fitcoeffs(self, val):
+        self.fitcoeffs.set(val)
+
     @staticmethod
     def name():
         return "XsecRecord"
@@ -140,6 +213,7 @@ class XsecRecord:
     def set(self, other):
         """ Sets this class according to another python instance of itself """
         if isinstance(other, XsecRecord):
+            lib.setVersionXsecRecord(self.__data__, int(other.version))
             lib.setSpeciesXsecRecord(self.__data__, int(other.spec))
             self.coeffs = other.coeffs
             self.ref_pressure = other.ref_pressure
@@ -148,6 +222,11 @@ class XsecRecord:
             self.xsecs = other.xsecs
             self.temperature_slope = other.temperature_slope
             self.temperature_intersect = other.temperature_intersect
+            self.fitminpressures = other.fitminpressures
+            self.fitmaxpressures = other.fitmaxpressures
+            self.fitmintemperatures = other.fitmintemperatures
+            self.fitmaxtemperatures = other.fitmaxtemperatures
+            self.fitcoeffs = other.fitcoeffs
         else:
             raise TypeError("Expects XsecRecord")
 
@@ -179,6 +258,7 @@ class XsecRecord:
 
     def __eq__(self, other):
         if isinstance(other, XsecRecord) and \
+                self.version == other.version and \
                 self.spec == other.spec and \
                 self.coeffs == other.coeffs and \
                 self.ref_pressure == other.ref_pressure and \
@@ -186,7 +266,12 @@ class XsecRecord:
                 self.fgrids == other.fgrids and \
                 self.xsecs == other.xsecs and \
                 self.temperature_slope == other.temperature_slope and \
-                self.temperature_intersect == other.temperature_intersect:
+                self.temperature_intersect == other.temperature_intersect and \
+                self.fitminpressures == other.fitminpressures and \
+                self.fitmaxpressures == other.fitmaxpressures and \
+                self.fitmintemperatures == other.fitmintemperatures and \
+                self.fitmaxtemperatures == other.fitmaxtemperatures and \
+                self.fitcoeffs == other.fitcoeffs:
             return True
         else:
             return False
@@ -209,6 +294,9 @@ lib.xmlreadXsecRecord.argtypes = [c.c_void_p, c.c_char_p]
 
 lib.xmlsaveXsecRecord.restype = c.c_long
 lib.xmlsaveXsecRecord.argtypes = [c.c_void_p, c.c_char_p, c.c_long, c.c_long]
+
+lib.getVersionXsecRecord.restype = c.c_long
+lib.getVersionXsecRecord.argtypes = [c.c_void_p]
 
 lib.getSpeciesXsecRecord.restype = c.c_long
 lib.getSpeciesXsecRecord.argtypes = [c.c_void_p]
@@ -236,3 +324,18 @@ lib.getTemperatureSlopeXsecRecord.argtypes = [c.c_void_p]
 
 lib.getTemperatureIntersectXsecRecord.restype = c.c_void_p
 lib.getTemperatureIntersectXsecRecord.argtypes = [c.c_void_p]
+
+lib.getFitMinPressuresXsecRecord.restype = c.c_void_p
+lib.getFitMinPressuresXsecRecord.argtypes = [c.c_void_p]
+
+lib.getFitMaxPressuresXsecRecord.restype = c.c_void_p
+lib.getFitMaxPressuresXsecRecord.argtypes = [c.c_void_p]
+
+lib.getFitMinTemperaturesXsecRecord.restype = c.c_void_p
+lib.getFitMinTemperaturesXsecRecord.argtypes = [c.c_void_p]
+
+lib.getFitMaxTemperaturesXsecRecord.restype = c.c_void_p
+lib.getFitMaxTemperaturesXsecRecord.argtypes = [c.c_void_p]
+
+lib.getFitCoeffsXsecRecord.restype = c.c_void_p
+lib.getFitCoeffsXsecRecord.argtypes = [c.c_void_p]
